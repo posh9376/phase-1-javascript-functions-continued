8c9cc3ce8ff708dc7a0631afd718306a54193395
Eric Richard <reric0802@gmail.com>
2024-10-05 05:33:52 +0300

    added code to ensure passing

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(work = 'go to the office') {
+    return (`This Monday, I will ${work}.`)
+}
+function wrapAdjective(sign = '*') {
+    const inner = function (word = 'special') {
+        return(`You are ${sign}${word}${sign}!`)
+    }
+    return inner;
+}
